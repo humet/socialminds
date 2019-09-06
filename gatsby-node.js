17c1e7bc39001bd15b1612a0c5f599835833739b
Ken Tsang <ken@mediachain.co>
2019-09-06 16:22:39 +0100

    updates

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,37 @@
  */
 
 // You can delete this file if you're not using it
+const path = require(`path`)
+const slash = require(`slash`)
+
+// Implement the Gatsby API “createPages”. This is
+// called after the Gatsby bootstrap is finished so you have
+// access to any information necessary to programmatically
+// create pages.
+// Will create pages for WordPress pages (route : /{slug})
+// Will create pages for WordPress posts (route : /post/{slug})
+exports.createPages = async ({ graphql }) => {
+
+  // The “graphql” function allows us to run arbitrary
+  // queries against the local Gatsby GraphQL schema. Think of
+  // it like the site has a built-in database constructed
+  // from the fetched data that you can run queries against.
+  const result = await graphql(`
+    {
+      allWordpressPost {
+        edges {
+          node {
+            id
+            path
+            status
+          }
+        }
+      }
+    }
+  `)
+
+  // Check for any errors
+  if (result.errors) {
+    throw new Error(result.errors)
+  }
+}
--- src/pages/episodes.js
+++ src/pages/episodes.js
@@ -2,12 +2,58 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import { StaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
 
 const EpisodesPage = () => (
   <Layout className="episodespage">
     <section className="episodes">
     <SEO title="Episodes" />
     <h1>All Episodes</h1>
+
+    <StaticQuery
+    query={graphql`
+      query EpisodesQuery {
+        allWordpressPost {
+          edges {
+            node {
+              title
+              acf {
+                episode_number
+                featuring
+              }
+              featured_media {
+                localFile {
+                  childImageSharp {
+                    fixed(width: 400, height: 400, cropFocus: CENTER) {
+                      ...GatsbyImageSharpFixed_withWebp_tracedSVG
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    `}
+    render={data => (
+      <div className="episodes__rows">
+        {data.allWordpressPost.edges.map((episode) => {
+          return (
+            <div className="episodes__blocks">
+              <div className="episodes__blocks-content">
+            <p>Episode {episode.node.acf.episode_number}</p>
+            <p>feat. {episode.node.acf.featuring}</p>
+            <h2>{episode.node.title}</h2>
+            <Img fixed={episode.node.featured_media.localFile.childImageSharp.fixed}/>
+            </div>
+            </div>
+          )
+        })}
+      </div>
+    )}
+  />
+
     </section>
   </Layout>
 )
